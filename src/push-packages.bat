e7c8d213d0971ba4077d924883720a06cad18632
Benoît Daireaux <beda@norceresearch.no>
2025-03-12 08:32:41 +0100

    Delete push-packages.bat

--- src/push-packages.bat
+++
@@ -1,6 +0,0 @@
-dotnet nuget push ./reference-implementation-packages/DWIS.API.DTO.2.1.21.nupkg --api-key oy2jpecxstvzqzwjesvayut6egzhrnou5ma6nirjirordy --source https://api.nuget.org/v3/index.json
-dotnet nuget push ./reference-implementation-packages/DWIS.OPCUA.Schemas.2.1.21.nupkg --api-key oy2jpecxstvzqzwjesvayut6egzhrnou5ma6nirjirordy --source https://api.nuget.org/v3/index.json
-dotnet nuget push ./reference-implementation-packages/DWIS.OPCUA.Vocabulary.2.1.21.nupkg --api-key oy2jpecxstvzqzwjesvayut6egzhrnou5ma6nirjirordy --source https://api.nuget.org/v3/index.json
-dotnet nuget push ./reference-implementation-packages/DWIS.Vocabulary.ResourceIntegration.2.1.21.nupkg --api-key oy2jpecxstvzqzwjesvayut6egzhrnou5ma6nirjirordy --source https://api.nuget.org/v3/index.json
-dotnet nuget push ./reference-implementation-packages/DWIS.SPARQL.Utils.2.1.21.nupkg --api-key oy2jpecxstvzqzwjesvayut6egzhrnou5ma6nirjirordy --source https://api.nuget.org/v3/index.json
-dotnet nuget push ./reference-implementation-packages/DWIS.OPCUA.Configuration.2.1.21.nupkg --api-key oy2jpecxstvzqzwjesvayut6egzhrnou5ma6nirjirordy --source https://api.nuget.org/v3/index.json
